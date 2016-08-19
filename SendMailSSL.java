5b3229ae5d49bb2c3ef93c200236a40faaa5de1d
pavankumar9 <pavan.kumar@goibibo.com>
2016-08-19 17:16:01 +0530

    Update SendMailSSL.java

--- SendMailSSL.java
+++ SendMailSSL.java
@@ -11,11 +11,11 @@ public class SendMailSSL {
 	public static void main(String[] args) {
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.port", "587");
 		props.put("mail.smtp.socketFactory.class",
 				"javax.net.ssl.SSLSocketFactory");
 		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
+		props.put("mail.smtp.port", "587");
 
 		Session session = Session.getDefaultInstance(props,
 			new javax.mail.Authenticator() {
